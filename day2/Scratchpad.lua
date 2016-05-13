a148862963f7f38cf90315acb31a8ce3aafd7cf5
Hybridtupel <marianbaeuerle@gmx.de>
2016-05-13 18:37:31 +0200

    Day 2 learning about tables

---
+++ day2/Scratchpad.lua
@@ -0,0 +1,84 @@
+--
+-- User: hybridtupel
+-- Date: 13.05.16
+-- Time: 14:18
+--
+
+book = {
+    title = "Grail Diary",
+    author = "Henry Jones",
+    pages = 100
+}
+
+print(book.title)
+print(book.stars)
+print(book.author)
+
+book.stars = 5
+book.author = "Henry Jones Sr."
+
+print(book.title)
+print(book.stars)
+print(book.author)
+
+key = "title"
+print(book[key])
+
+function print_table(t)
+    for k, v in pairs(t) do
+        print(k .. ":  " .. v)
+    end
+end
+
+print()
+
+print_table(book)
+
+medals = {
+    "gold",
+    "silver",
+    "bronze"
+}
+
+print()
+
+print(medals[1])
+medals[4] = "lead"
+
+print()
+
+ice_cream_scoops = {
+    "vanilla",
+    "chocholate";
+
+    sprinkles = true
+}
+
+print(ice_cream_scoops[1])
+print(ice_cream_scoops.sprinkles)
+
+function table_to_string(t)
+    local result = {}
+
+    for k, v in pairs(t) do
+        result[#result + 1] = k .. ": " .. v
+    end
+
+    return table.concat(result, "\n")
+end
+
+greek_numbers = {
+    ena = "one",
+    dyo = "two",
+    tria = "three"
+}
+
+mt = {
+    __tostring = table_to_string
+}
+
+setmetatable(greek_numbers, mt)
+
+print("\nGreek numbers")
+
+print(greek_numbers)
---
+++ day2/strict.lua
@@ -0,0 +1,37 @@
+--
+-- User: hybridtupel
+-- Date: 13.05.16
+-- Time: 16:21
+--
+
+local _private = {}
+
+function strict_read(table, key)
+    if _private[key] then
+        return _private[key]
+    else
+        error("Invalid key: " .. key)
+    end
+end
+
+function strict_write(table, key, value)
+    if _private[key] then
+        error("Duplicate key: " .. key)
+    else
+        _private[key] = value
+    end
+end
+
+local mt = {
+    __index = strict_read,
+    __newindex = strict_write
+}
+
+treasure = {}
+setmetatable(treasure, mt)
+
+treasure.gold = 50
+
+print(treasure.gold)
+
+treasure.gold = 100
