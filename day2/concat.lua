68197e2381925d0d27e5db2eba77ec0f63871e5c
Hybridtupel <marianbaeuerle@gmx.de>
2016-05-16 22:10:59 +0200

    Fixed wrong implementation of concat. Error when adding two tables remains

--- day2/concat.lua
+++ day2/concat.lua
@@ -5,15 +5,23 @@
 --
 
 function concatenate(a1, a2)
-    local result = a1
-    for _,v in ipairs(a2) do
-        a1[#a1 + 1] = v
+    local result = {}
+    for i, v in ipairs(a1) do
+        result[i] = v
+    end
+    for _, v in ipairs(a2) do
+        result[#result + 1] = v
     end
     return result
 end
 
+_G.__newindex = function(table, key, value)
+    setmetatable(table, { __add = concatenate })
+end
+
+
 a1 = {
-    1, 3, 3, 5
+    1, 2, 3, 4
 }
 a2 = {
     5, 6, 7, 8
@@ -21,6 +29,16 @@ a2 = {
 
 a3 = concatenate(a1, a2)
 
-for i,v in ipairs(a3) do
+for i, v in ipairs(a3) do
+    print(i .. ": " .. v)
+end
+
+
+
+a4 = a1 + a2
+
+print("-----")
+
+for i, v in ipairs(a4) do
     print(i .. ": " .. v)
 end
